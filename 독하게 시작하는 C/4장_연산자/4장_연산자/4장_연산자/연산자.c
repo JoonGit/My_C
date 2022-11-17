1932b82ae78a6933a42f725629daa8c8b3d11ce2
LeejaeJoon <ljj9730@gmail.com>
2022-11-17 23:08:56 +0900

    221116 제어문까지

--- 독하게 시작하는 C/4장_연산자/4장_연산자/4장_연산자/연산자.c
+++ 독하게 시작하는 C/4장_연산자/4장_연산자/4장_연산자/연산자.c
@@ -3,50 +3,89 @@
 int main(void) {
 	//int num1, num2;
 	//printf("두 정수를 입력하세요.:");
-	//scanf_s("%d %d", &num1, &num2);
+	//scanf_s("%d%d", &num1, &num2);
 	//double AVG = (num1 + num2) / 2;
 	//printf("AVG : %.2lf", AVG);
 
-	//// 시간 계산
+	// 시간 계산
+	/*int time;
+	scanf_s("%d", &time);
+	int h = time / 3600;
+	int m_ = time - (h * 3600);
+	int m = m_ / 60;
+	int s = m_ - (m * 60);
+	if (h < 10) {		
+		if (m < 10) {			
+			if (s < 10) {
+				printf("%d초는 0%d시간 0%d분 0%d초 입니다", time, h, m, s);
+				return;
+			}
+			printf("%d초는 0%d시간 0%d분 %d초 입니다", time, h, m, s);
+			return;
+		}
+		printf("%d초는 0%d시간 %d분 %d초 입니다", time, h, m, s);
+	}
+	else if (m < 10) {
+		if (s < 10) {
+			printf("%d초는 %d시간 0%d분 %d초 입니다", time, h, m, s);
+			return;
+		}
+		printf("%d초는 %d시간 0%d분 0%d초 입니다", time, h, m, s);
+	}
+	else if(s < 10) {
+		printf("%d초는 %d시간 %d분 0%d초 입니다", time, h, m, s);
+	}
+	else {
+		printf("%d초는 %d시간 %d분 %d초 입니다", time, h, m, s);
+	}*/
+
 	//int time;
 	//scanf_s("%d", &time);
 	//int h = time / 3600;
 	//int m_ = time - (h * 3600);
 	//int m = m_ / 60;
 	//int s = m_ - (m * 60);
-	//if (h < 10) {		
-	//	if (m < 10) {			
-	//		if (s < 10) {
-	//			printf("%d초는 0%d시간 0%d분 0%d초 입니다", time, h, m, s);
-	//			return;
-	//		}
-	//		printf("%d초는 0%d시간 0%d분 %d초 입니다", time, h, m, s);
-	//		return;
-	//	}
-	//	printf("%d초는 0%d시간 %d분 %d초 입니다", time, h, m, s);
-	//}
-	//else if (m < 10) {
-	//	if (s < 10) {
-	//		printf("%d초는 %d시간 0%d분 %d초 입니다", time, h, m, s);
-	//		return;
-	//	}
-	//	printf("%d초는 %d시간 0%d분 0%d초 입니다", time, h, m, s);
-	//}
-	//else if(s < 10) {
-	//	printf("%d초는 %d시간 %d분 0%d초 입니다", time, h, m, s);
-	//}
-	//else {
-	//	printf("%d초는 %d시간 %d분 %d초 입니다", time, h, m, s);
-	//}
+	//printf("%d초는 %02d시간 %02d분 %02d초 입니다",
+	//	time,
+	//	h,
+	//	m,
+	//	s);
 
-	int nInput, nTotal = 0;
+	//int nInput, nTotal = 0;
+	//scanf_s("%d", &nInput);
+	//nTotal += nInput;
+	//scanf_s("%d", &nInput);
+	//nTotal += nInput;
+	//scanf_s("%d", &nInput);
+	//nTotal += nInput;
+	//
+	//printf("Total : %d\n", nTotal);
+
+	//int nInput = 0;
+	//scanf_s("%d", &nInput);
+	//printf(nInput >= 150 ? "합격" : "불합격");
+
+	//int nMax = 0;
+	//int a, b, c;
+	//scanf_s("%d%d%d", &a, &b, &c);
+	//a >= b ? (nMax = a) : (nMax = b);
+	//nMax < c ? (nMax = c) : nMax;
+	//printf("MAX : %d\n", nMax);
+
+	int nMax = -100;
+	int nInput = 0;
+
+
 	scanf_s("%d", &nInput);
-	nTotal += nInput;
+	nMax = nInput > nMax ? nInput : nMax;
+
 	scanf_s("%d", &nInput);
-	nTotal += nInput;
+	nMax = nInput > nMax ? nInput : nMax;
+
 	scanf_s("%d", &nInput);
-	nTotal += nInput;
+	nMax = nInput > nMax ? nInput : nMax;
+
+	printf("MAX : %d\n", nMax);
 	
-	printf("Total : %d\n", nTotal);
  	return 0;
 }
--- 독하게 시작하는 C/6장_제어문/06_제어문/06_제어문/제어문.c
+++ 독하게 시작하는 C/6장_제어문/06_제어문/06_제어문/제어문.c
@@ -1,20 +1,44 @@
 #include <stdio.h>
 
 int main(void) {
+	//int nInput, nMax = 0;
+
+	//scanf_s("%d", &nInput);
+	//if (nInput > nMax) nMax = nInput;
+
+	//scanf_s("%d", &nInput);
+	//if (nInput > nMax) nMax = nInput;
+
+	//scanf_s("%d", &nInput);
+	//if (nInput > nMax) nMax = nInput;
+
+	//printf("MAX : %d", nMax);
+
+	//int nAge = 0;
+	//int nPay = 1000;
+	//int nDiscount = -250;
+
+	//scanf_s("%d", &nAge);
+	//
+	//if (nAge <= 19) printf("나이 %d 최종요금 : %d", nAge, nPay + nDiscount);
+	//else printf("나이 %d 최종요금 : %d", nAge, nPay);
+
 	const int bus_pay = 1000;
 	int pay;
 	int age = 0;
 	scanf_s("%d", &age);
 
 	if (age <= 13) {
-		pay = bus_pay * 0.50;
+		pay = bus_pay * 0.5;
 		if (age <= 3) 
-			pay = bus_pay * 0;
+			pay =  0;
 	}
 	else if (age >= 13) {
 		pay = bus_pay * 0.75;
-			if (age >= 20)
-			pay = bus_pay * 1;		
+		if (age >= 60) pay = 0;
+		else if (age >= 20) {
+			pay = bus_pay;
+		}
 	}
 	printf("나이 : %d\n", age);
 	printf("금액 : %d\n", pay);
