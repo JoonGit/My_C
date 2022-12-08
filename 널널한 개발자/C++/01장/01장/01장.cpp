cd10fa79c5721a5329a072dcdfd3926370d024b1
LeejaeJoon <ljj9730@gmail.com>
2022-12-08 22:41:44 +0900

    221208 C++ 03장 클래스

---
+++ 널널한 개발자/C++/01장/01장/01장.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include "stdafx.h"
+
+
+int _tmain(int argc, _TCHAR* argv[]) {
+	std::cout;
+	return 0;
+}
---
+++ 널널한 개발자/C++/03장/03장/03장.cpp
@@ -0,0 +1,55 @@
+﻿#include <iostream>
+using namespace std;
+
+class CTest
+{
+public:
+    CTest()
+    {
+        m_nData = 10;
+    }
+
+    int m_nData;
+
+    void PrintData(void);
+};
+
+void CTest::PrintData(void)
+{
+    cout << m_nData << endl;
+}
+
+class CMyData
+{
+
+    int m_nData;
+
+public:
+    CMyData() {};
+    int GetData(void) { return m_nData; }
+    void SetData(int nParam) { m_nData = nParam; }
+};
+
+class CRefTest
+{
+public:
+    CRefTest(int &rParam) : m_nData(rParam) {};
+    int GetData(void) { return m_nData; }
+
+private:
+    int &m_nData;
+};
+
+int main()
+{
+    int a = 10;
+    CRefTest t(a);
+
+    cout << t.GetData() << endl;
+
+    a = 20;
+    cout << t.GetData() << endl;
+
+    return 0;
+}
+
